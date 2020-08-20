5aa76383edba26a8937f742365792180c8237965
Johnny Li <reptile18@gmail.com>
2020-08-19 19:03:57 -0700

    hardcode basename in browserrouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="https://reptile18.github.io/">
       {console.log(`process.env.PUBLIC_URL: ${process.env.PUBLIC_URL}`)}
       <div className="App">
         <Navbar></Navbar>
