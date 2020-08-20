92da9af1a8de0ae2744cd6651216098d705c8239
Johnny Li <reptile18@gmail.com>
2020-08-19 19:08:13 -0700

    undo hardcoding of basename in browserrouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="https://reptile18.github.io/">
+    <Router basename={process.env.PUBLIC_URL}>
       {console.log(`process.env.PUBLIC_URL: ${process.env.PUBLIC_URL}`)}
       <div className="App">
         <Navbar></Navbar>
