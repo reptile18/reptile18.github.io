f19a1fd29604665ed1ab7eb3702d8d0218db1591
Johnny Li <reptile18@gmail.com>
2020-08-21 14:28:41 -0700

    fixes for responsiveness

--- src/components/MobileFirst/index.js
+++ src/components/MobileFirst/index.js
@@ -3,6 +3,7 @@ import {Grid} from '@material-ui/core';
 import VizSensor from 'react-visibility-sensor';
 import Fade from '@material-ui/core/Fade';
 import Grow from '@material-ui/core/Grow';
+import './style.css';
 
 const styles = {
   container: {
@@ -16,7 +17,6 @@ const styles = {
     color: "white"
   },
   mobiles: {
-    width: "20%",
   }
 }
 
@@ -30,14 +30,14 @@ function MobileFirst() {
         <VizSensor onChange={(isVisible) => setShowMobiles(isVisible)}>
           <Grow in={showMobiles} timeout={1000}>
             <Fade in={showMobiles} timeout={1000}>
-              <img style={styles.mobiles} src="./assets/pixel2_ccidi.png" alt="covid can i do it, on mobile"></img>
+              <img className="mobile" style={styles.mobiles} src="./assets/pixel2_ccidi.png" alt="covid can i do it, on mobile"></img>
             </Fade>
           </Grow>
         </VizSensor>
         <VizSensor onChange={(isVisible) => setShowMobiles(isVisible)}>
           <Grow in={showMobiles} timeout={1000}>
             <Fade in={showMobiles} timeout={1000}>
-              <img style={styles.mobiles} src="./assets/pixel2_sttl.png" alt="skip to the loo, on mobile"></img>
+              <img className="mobile" style={styles.mobiles} src="./assets/pixel2_sttl.png" alt="skip to the loo, on mobile"></img>
             </Fade>
           </Grow>
         </VizSensor>
